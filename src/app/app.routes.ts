321936291061b9ed8eaff2afa9a02f35912f3cd8
Caleb <11calebgraham@gmail.com>
2024-01-06 15:20:52 -0600

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SummaryComponent } from './summary/summary.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        children: [
+            {
+                path: '',
+                component: HomeComponent,
+            },
+            {
+                path: 'summary', 
+                component: SummaryComponent
+            }
+        ]
+    }
+];
