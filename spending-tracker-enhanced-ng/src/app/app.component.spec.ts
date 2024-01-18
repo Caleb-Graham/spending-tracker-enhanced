4b2c665f3ca7fc4d13605502b8d7c42b890a8fd0
Caleb <11calebgraham@gmail.com>
2024-01-17 19:41:50 -0600

    create spending-tracker-enhanced front and backend

--- spending-tracker-enhanced-ng/src/app/app.component.spec.ts
+++ spending-tracker-enhanced-ng/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'spending-tracker-enhanced' title`, () => {
+  it(`should have the 'spending-tracker-enhanced-ng' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('spending-tracker-enhanced');
+    expect(app.title).toEqual('spending-tracker-enhanced-ng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, spending-tracker-enhanced');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, spending-tracker-enhanced-ng');
   });
 });
